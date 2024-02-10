f8decbf633619c79d7c21006888d50e7314722cd
100opps <c0ntact100opps@gmail.com>
2024-02-10 01:41:57 -0300

    feat: presence added to bot

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,3 +1,13 @@
+import { ActivityType } from 'discord.js'
+
 export default async (client) => {
+  const presenceOptions = {
+    status: 'dnd',
+    activities: [{
+      name: `${client.guilds.cache.size} servers and ${client.guilds.cache.reduce((totalMembers, guild) => totalMembers + guild.memberCount, 0)} users`,
+      type: ActivityType.Watching
+    }]
+  }
+  client.user.setPresence(presenceOptions)
   console.log(`logged in as ${client.user.tag}`)
 }
