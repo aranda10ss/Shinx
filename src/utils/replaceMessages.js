d7492e00d5167bcb76b7bac97cf1bb1f511d7542
100opps <c0ntact100opps@gmail.com>
2024-02-14 02:35:00 -0300

    ✨ feat: Add new function `replacedMessages`

---
+++ src/utils/replaceMessages.js
@@ -0,0 +1,10 @@
+function replaceMessage (text, message) {
+  const user = message.author ? message.author : message
+
+  return text
+    .replace(/\[members\]/g, message.guild.memberCount)
+    .replace(/\[serverName\]/g, message.guild.name)
+    .replace(/\[user\]/g, user)
+}
+
+export default replaceMessage
